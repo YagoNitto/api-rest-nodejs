b817def997e586e3be1ee10bad1c8762f854765b
Yago <yago_nitto@hotmail.com>
2024-08-27 18:52:01 -0300

    create get routes and summary

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoute } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions')
-    .where('amount', 1000)
-    .select('*')
-
-  return transactions
+app.register(transactionsRoute, {
+  prefix: 'transactions',
 })
 
 app.listen({ port: env.PORT }).then(() => {
